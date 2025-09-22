8331a89328c135811df40b7e9ba0ec6852b6acff
Christofferns <144909120+Christofferns@users.noreply.github.com>
2025-09-22 13:48:33 +0200

    "edit mulighet"

--- Controllers/ObstacleController.cs
+++ Controllers/ObstacleController.cs
@@ -15,17 +15,31 @@ namespace FirstWebApplication.Controllers
         [HttpPost]
         public IActionResult DataForm(ObstacleData obstacleData)
         {
-            // Sjekker attributtene på modellen
             if (!ModelState.IsValid)
-            {
-                // Ugyldig -> vis skjemaet igjen med feilmeldinger
                 return View(obstacleData);
-            }
 
-            // Her vil obstacleData.GeometryGeoJson inneholde en GeoJSON FeatureCollection (string)
-            // Klar til å lagres i DB om du ønsker.
+            // Ferdig utfylt -> vis Overview
+            return View("Overview", obstacleData);
+        }
 
-            // Gyldig -> gå til oversikt med data
+        // ----- EDIT -----
+
+        // Vi laster Edit-skjemaet ved å POSTe hele modellen fra Overview (skåner oss for for lang querystring).
+        [HttpPost]
+        public IActionResult Edit(ObstacleData obstacleData)
+        {
+            // Viser Edit-skjema med eksisterende verdier (inkl. GeoJSON)
+            return View("Edit", obstacleData);
+        }
+
+        // Lagre endringer fra Edit-skjemaet og vis Overview igjen
+        [HttpPost]
+        public IActionResult EditSave(ObstacleData obstacleData)
+        {
+            if (!ModelState.IsValid)
+                return View("Edit", obstacleData);
+
+            // Tilbake til Overview med oppdatert modell
             return View("Overview", obstacleData);
         }
     }
